6c024e52feb78e7b0df1c0d07356fa0dc3a79d8a
Fares-7 <faresghandour718@gmail.com>
2025-01-28 18:41:05 +0200

    first

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * 
+ *     Class & Objects 
+ * 
+ *  ex) Blog Website 
+ *      class = code to add new post , news , ...
+ *      object = post , news , ...
+ * 
+ */
+
+ class blog {
+
+    // Properties
+    // variable inside the class = property
+    public $title;
+    public $content;
+    public $auther;
+ }
+
+
+ // variable outside the class = variable
+ $object1  = new blog();
+
+ echo "<pre>";
+ print_r($object1);
+ echo "</pre>";
